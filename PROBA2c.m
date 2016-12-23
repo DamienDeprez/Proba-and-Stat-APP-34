95173e597624d5a35a0ebb444c4ff33d197e8b50
ASLITI <abbassliti@msn.com>
2016-12-23 13:04:19 +0100

    Update PROBA2c.m

--- PROBA2c.m
+++ PROBA2c.m
@@ -1,17 +1,26 @@
-%% -----------------------1(f)----------------------------
 a=2;       b=4; %Parameters
-n=20;
-U=zeros(1,n);
-for i=1:n
-    U(i)=unifrnd(0,1);
-end
-tt=sqrt(1-(1-U).^(1/4)) ;
+N=100;          Mtot=zeros(3,N);
+for j=1:N
+    n=20;
+    U=zeros(1,n);
+    for i=1:n
+        U(i)=unifrnd(0,1);
+    end
+    tt=sqrt(1-(1-U).^(1/4)) ;
+    BM=n/sum(tt.^a)-1;   %Estimator for method of moments
+    W=zeros(1,20);
+    for i=1:n
+        W(i)=log(1-tt(i)^a);
+    end
+    BL=-n/sum(W);       %Estimator for MLE
+    %Calcul of the mediane and the 3 estimators
 
-u=mean(tt);
-Med=median(tt);
-V=var(tt);
-IQR=iqr(tt);
-cv=sqrt(V)/u;
+    E1=quantile(tt,0.95);
+    E2=(1-0.05^(1/BM))^(1/a);
+    E3=(1-0.05^(1/BL))^(1/a);
+    M=[E1 E2 E3];
+    Mtot(:,j)=M ; 
+end
 %% ----(c) Estimate the bias, the variance and the mean squared-----------
 theta = 0.726;          %exact value of the estimator
 B=[mean(Mtot(1,:))-theta;     %Calcul of the Bias of 3 estimators
